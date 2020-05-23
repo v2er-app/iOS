07b1ef423214d8d25de2f7c901ae602b97564051
SethCreate <sethcreate@outlook.com>
2020-05-23 18:47:07 +0800

    init commit

---
+++ V2er/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  V2er
+//
+//  Created by Seth on 2020/5/23.
+//  Copyright © 2020 lessmore.io. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
